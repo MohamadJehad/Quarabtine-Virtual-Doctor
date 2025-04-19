a0d2fdfdbd86158bfb87c1f33d2e65980252df5f
MohamadJehad <mohamad.jehad@remotecoders.org>
2025-04-19 23:26:18 +0200

    Update itManagerController.js

--- server/controllers/itManagerController.js
+++ server/controllers/itManagerController.js
@@ -4,6 +4,10 @@ const Nurse = require('../models/nurseModel');
 const Patient = require('../models/patientModel');
 const Receptionist = require('../models/receptionistModel');
 
+exports.testRoute = (req, res) => {
+  res.send('IT Manager test route works!');
+};
+
 exports.renderAddITManagerForm = async (res) => {
   try {
     res.render('it-manager/add');
@@ -33,14 +37,14 @@ exports.addITManager = async (req, res) => {
   }
 };
 
-exports.renderITManagerHome = async (res) => {
+exports.renderITManagerHome = async (_, res) => {
   try {
     const itManagers = await ITManager.getAll();
     const doctors = await Doctor.getAll();
     const patients = await Patient.getAll();
     const nurses = await Nurse.getAll();
     const receptionists = await Receptionist.getAll();
-
+    
     res.render('it-manager/home', {
       IT_Manager: itManagers,
       doctors,
@@ -51,6 +55,51 @@ exports.renderITManagerHome = async (res) => {
   } catch (error) {
     console.error('Error rendering IT manager home:', error);
     res.status(500).render('errors/500', { error: 'Failed to load IT manager home' });
+  }
+};
+
+exports.renderAddDoctorForm = async (req, res) => {
+  try {
+    // Render the add doctor form view
+    res.render('it-manager/add-doctor');
+  } catch (error) {
+    console.error('Error rendering add doctor form:', error);
+    res.status(500).render('errors/500', { error: 'Failed to load add doctor form' });
+  }
+};
+
+exports.addDoctor = async (req, res) => {
+  try {
+    const { 
+      FName, 
+      gender, 
+      username, 
+      password, 
+      specialization, 
+      mobile, 
+      city, 
+      street, 
+      buildingNo 
+    } = req.body;
+
+    // Create doctor
+    const doctorId = await Doctor.create({
+      FName,
+      gender: req.body.Male ? 'Male' : 'Female',
+      username,
+      password,
+      specialization,
+      mobile,
+      city,
+      street,
+      buildingNo
+    });
+
+    // Redirect to IT manager home page after successful creation
+    res.redirect('/it-manager/home');
+  } catch (error) {
+    console.error('Error adding doctor:', error);
+    res.status(500).render('errors/500', { error: 'Failed to add doctor' });
   }
 };
 
